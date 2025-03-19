1b3038e7ef29c2e7d915304e51d1ede9ba2acb27
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2025-03-19 14:12:34 +0900

    animate processing subscription icon

--- app/network/Main/Connect/ConnectButton/ConnectProcessingSubscription/ConnectProcessingSubscriptionView.swift
+++ app/network/Main/Connect/ConnectButton/ConnectProcessingSubscription/ConnectProcessingSubscriptionView.swift
@@ -10,14 +10,28 @@ import SwiftUI
 struct ConnectProcessingSubscriptionView: View {
     
     @EnvironmentObject var themeManager: ThemeManager
+
+    @State private var opacity: CGFloat = 0
     
     var body: some View {
         VStack {
             Image(systemName: "hourglass")
                 .font(.system(size: 32))
                 .foregroundColor(themeManager.currentTheme.textFaintColor)
+                .opacity(opacity)
         }
         .padding(.horizontal, 4)
+        .onAppear {
+            // Delay the animation by 500ms
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                withAnimation(.easeInOut(duration: 0.3)) {
+                    opacity = 1
+                }
+            }
+        }
+        .onDisappear {
+            opacity = 0
+        }
     }
 }
 
